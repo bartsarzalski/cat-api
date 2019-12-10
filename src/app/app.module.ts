8c56fa30ae9ae1e92c1fff0f9e152c3d128b42cd
Bartek <bartek@macbook-pro-bartek.home>
2019-12-10 09:05:15 +0100

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BreedsComponent } from './breeds/breeds.component';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BreedsService } from './services/breeds.service';
-import { ImageService } from './services/image.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -19,7 +17,6 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     LoginComponent,
     BreedsComponent,
-    NavbarComponent,
     NotFoundComponent,
   ],
   imports: [
@@ -37,8 +34,7 @@ import { HttpClientModule } from '@angular/common/http';
 
   ],
   providers: [
-    BreedsService,
-    ImageService
+    BreedsService
   ],
   bootstrap: [AppComponent]
 })
